6be09b1c0abdc219f1449e9c329b0e4e9c4fc7fe
Passport Parking <vagrant@passport-vagrant-image>
2015-06-12 17:01:17 +0000

    added colors to log output. red for errors. underline for filtering. bold for highlighting.

--- app/js/passport.js
+++ app/js/passport.js
@@ -26,3 +26,38 @@ function pauseButtonClickHandler() {
     }
 }
 
+var lineRegex = /((\w*)(->|::|GLOBAL)(\w*))(\[)(.*)(:)(\d*)(\])(:)((.*)( =>)(.*))?(.*)/;
+
+function buildLine(line) {
+    var newline = [];
+    newline.push('<span>');
+    var matches = line.match(lineRegex);
+    if (matches && matches.length) {
+        if (matches[1] === 'GLOBAL' && matches[3] === 'GLOBAL') {
+            newline.push('<span class="color_turquoise">GLOBAL</span>');
+        } else {
+            newline.push('<span class="color_emerland">', matches[2], '</span>');
+            newline.push('<span>', matches[3], '</span>');
+            newline.push('<span class="color_peterriver">', matches[4], '</span>');
+        }
+        newline.push('<span>', matches[5], '</span>');
+        newline.push('<span class="color_orange">', matches[6], '</span>');
+        newline.push('<span>', matches[7], '</span>');
+        newline.push('<span class="color_wetasphalt">', matches[8], '</span>');
+        newline.push('<span>', matches[9], '</span>');
+        if (!matches[11]) {
+            newline.push('<span class="color_pomegranate">', matches[10], '</span>');
+            newline.push('<span class="color_midnightblue">', matches[15], '</span>');
+        } else {
+            newline.push('<span>', matches[10], '</span>');
+            newline.push('<span class="color_amethyst">', matches[12], '</span>');
+            newline.push('<span>', matches[13], '</span>');
+            newline.push('<span class="color_greensea">', matches[14], '</span>');
+        }
+    } else {
+        newline.push(line);
+    }
+    newline.push('</span>');
+    return newline.join('');
+}
+
